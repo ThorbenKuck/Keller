639304dc369e8931fe42deccd54aef7d14b94501
thorben <thorben.kuck@gmail.com>
2018-02-22 16:30:47 +0100

    added Annotations

---
+++ src/main/java/de/thorbenkuck/keller/annotations/Synchronized.java
@@ -0,0 +1,17 @@
+package de.thorbenkuck.keller.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * This annotation states that an Class is Synchronized and does not do anything inside of other Threads at any point in
+ * time if any Method is called.
+ *
+ * So it is not possible to have an class be annotated with @Synchronized and any method with {@link Asynchronous}.
+ *
+ * Future: Create an Annotation-Processor, that ensures that no Method is annotated with {@link Asynchronous}
+ */
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Synchronized {
+}
