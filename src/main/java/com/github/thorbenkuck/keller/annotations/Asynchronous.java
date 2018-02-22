5f8cd25b4311e5530c51ed9beb16d64ab11e6950
thorben <thorben.kuck@gmail.com>
2018-02-22 17:13:23 +0100

    release preparation

--- src/main/java/com/github/thorbenkuck/keller/annotations/Asynchronous.java
+++ src/main/java/com/github/thorbenkuck/keller/annotations/Asynchronous.java
@@ -6,9 +6,6 @@ import java.lang.annotation.*;
  * This annotation is to used to signal, that a method-call is executing whatever it wants to do, within another Thread.
  *
  * This should be a sign for anyone, to ensure thread safety
- *
- * Outside of NetCom2s internal Modules, this Annotation is a signal, that you will be seduced to asynchronous behaviour
- * if you call this Method. Therefor you may not be able to work synchronous or in an procedural style.
  */
 @Documented
 @Retention(RetentionPolicy.SOURCE)
